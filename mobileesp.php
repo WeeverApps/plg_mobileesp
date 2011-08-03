708dc13a5d1fa9d48145a776c01a911673ef9187
Robert Gerald Porter <rob@cartanova.ca>
2011-08-03 11:58:01 -0400

    First commit

--- mobileesp.php
+++ mobileesp.php
@@ -7,17 +7,17 @@
 *	Authors: 	Robert Gerald Porter (Joomla! Plugin) <rob@weeverapps.com>
 *				Anthony Hand (The MobileESP Project) <http://code.google.com/p/mobileesp/>		
 *	Version: 	0.9.1
-*   License: 	GPL v3.0
+*	License: 	GPL v3.0
 *
-*   This extension is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
+*	This extension is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
 *
-*   This extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details <http://www.gnu.org/licenses/>.
+*	This extension is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details <http://www.gnu.org/licenses/>.
 * 
 */
 
